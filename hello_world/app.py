288e55340d040d8736276c69f73b589dcd4b92f3
psomareddy <prakash@cloudmasters.biz>
2025-10-25 22:10:12 -0400

    instrument to add attributes

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,6 +1,7 @@
 import json
 import logging
 
+import requests
 from opentelemetry import trace
 
 logger = logging.getLogger(__name__)
@@ -11,7 +12,9 @@ tracer = trace.get_tracer(__name__)
 
 def lambda_handler(event, context):
     logger.info(f'Calling out to my sample lamb handler v2')
-    do_work()
+    current_span = trace.get_current_span()
+    current_span.set_attribute("r-version", "1.5.0")
+    check_ip()
     return {
         "statusCode": 200,
         "body": json.dumps(
@@ -21,6 +24,14 @@ def lambda_handler(event, context):
         ),
     }
 
-@tracer.start_as_current_span("do_work")
-def do_work():
-    logger.info(f'Executing method do_work')
+@tracer.start_as_current_span("check_ip")
+def check_ip():
+    logger.info(f'Start executing check_ip method ')
+    current_span = trace.get_current_span()
+    try:
+        ip = requests.get('http://checkip.amazonaws.com')
+        current_span.set_attribute("ip.address", ip.text)
+    except requests.exceptions.RequestException as e:
+        logger.error(e)
+        raise e
+    logger.info(f'End executing check_ip method ')
